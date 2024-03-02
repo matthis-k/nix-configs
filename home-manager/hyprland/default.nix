275008b57287e80588645e51fb480c815f303a25
matthis-k <matthis.kaelble@gmail.com>
2024-03-02 06:03:28 +0100

    refactor: extract hyprland settings to submodule

--- home-manager/hyprland/default.nix
+++ home-manager/hyprland/default.nix
@@ -6,12 +6,7 @@
 }: {
   imports = [
     ./hyprlock.nix
-  ];
-  wayland.windowManager.hyprland.enable = true;
-  wayland.windowManager.hyprland.settings = (import ./settings.nix) {inherit pkgs color;};
-  wayland.windowManager.hyprland.package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-  wayland.windowManager.hyprland.plugins = with pkgs; [
-    inputs.hycov.packages.${pkgs.system}.hycov
+    ./hyprland.nix
   ];
 
   programs.waybar.enable = true;
@@ -27,83 +22,6 @@
     unload = /home/matthisk/.config/wallpaper.jpg
     splash = false
   '';
-
-  programs.hyprlock = {
-    enable = true;
-
-    general = {
-      disable_loading_bar = false;
-      grace = 5;
-      hide_cursor = true;
-      no_fade_in = false;
-    };
-    backgrounds = [
-      {
-        monitor = "";
-        path = "screenshot";
-        color = "";
-        blur_size = 5;
-        blur_passes = 4;
-        noise = 0.0117;
-        contrast = 0.8917;
-        vibrancy = 0.1686;
-        vibrancy_darkness = 0.05;
-      }
-    ];
-    input-fields = [
-      {
-        monitor = "";
-        size = {
-          width = 200;
-          height = 50;
-        };
-        outline_thickness = 3;
-        dots_size = 0.33;
-        dots_spacing = 0.15;
-        dots_center = true;
-        outer_color = "rgb(151515)";
-        inner_color = "rgb(200,200,200)";
-        font_color = "rgb(10,10,10)";
-        fade_on_empty = true;
-        placeholder_text = "<i>Input Password...</i>";
-        hide_input = false;
-        position = {
-          x = 0;
-          y = -20;
-        };
-        halign = "center";
-        valign = "center";
-      }
-    ];
-    labels = [
-      {
-        monitor = "";
-        text = "<b>$TIME</b>";
-        color = "rgb(200, 200, 200)";
-        font_size = 50;
-        font_family = "Hack Nerd Font Mono";
-        position = {
-          x = 0;
-          y = 130;
-        };
-        halign = "center";
-        valign = "center";
-      }
-      {
-        monitor = "";
-        text = "Hi there, $USER";
-        color = "rgb(200, 200, 200)";
-        font_size = 25;
-        font_family = "Hack Nerd Font Mono";
-        position = {
-          x = 0;
-          y = 80;
-        };
-        halign = "center";
-        valign = "center";
-      }
-    ];
-  };
 
   home.packages = with pkgs; [
     blueberry
