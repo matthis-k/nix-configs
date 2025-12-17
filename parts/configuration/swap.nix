8262b0c9737791b0ac4520eb4000e83a158469cd
matthis-k <matthis.kaelble@gmail.com>
2025-12-17 08:36:00 +0100

    fix desktop swap sapce size

--- parts/configuration/swap.nix
+++ parts/configuration/swap.nix
@@ -1,11 +1,16 @@
 {
   nixos =
-    { ... }:
+    { hostMachine, ... }:
     {
       swapDevices = [
         {
           device = "/var/lib/swapfile";
-          size = 16 * 1024;
+          size =
+            {
+              laptop = 16 * 1024;
+              desktop = 32 * 1024;
+            }
+            .${hostMachine};
         }
       ];
     };
