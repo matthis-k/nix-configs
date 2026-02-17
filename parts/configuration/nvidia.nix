489619437821d228e3e8fd6400ce536c4b47200a
matthis-k <matthis.kaelble@gmail.com>
2026-02-17 12:57:47 +0100

    supposed to be

--- parts/configuration/nvidia.nix
+++ parts/configuration/nvidia.nix
@@ -2,7 +2,7 @@
   nixos =
     { config, lib, ... }:
     {
-      boot = lib.mkIf (config.hostName == "desktop") {
+      boot = lib.mkIf (config.hostMachine == "desktop") {
         kernelParams = [ "nvidia-drm.modeset=1" ];
         initrd.kernelModules = [
           "nvidia"
@@ -12,19 +12,19 @@
         ];
       };
 
-      hardware.graphics = lib.mkIf (config.hostName == "desktop") {
+      hardware.graphics = lib.mkIf (config.hostMachine == "desktop") {
         package = config.hardware.nvidia.package;
         enable32Bit = true;
         package32 = config.hardware.nvidia.package.lib32;
       };
 
-      hardware.nvidia = lib.mkIf (config.hostName == "desktop") {
+      hardware.nvidia = lib.mkIf (config.hostMachine == "desktop") {
         modesetting.enable = true;
         powerManagement.enable = true;
         open = true;
         nvidiaSettings = true;
       };
 
-      services.xserver.videoDrivers = lib.mkIf (config.hostName == "desktop") [ "nvidia" ];
+      services.xserver.videoDrivers = lib.mkIf (config.hostMachine == "desktop") [ "nvidia" ];
     };
 }
--- parts/hyprland/default.nix
+++ parts/hyprland/default.nix
@@ -11,7 +11,7 @@
       environment.systemPackages =
         with pkgs;
         [
-          #grimblast
+          grimblast
           hyprpolkitagent
           satty
           wl-clipboard
